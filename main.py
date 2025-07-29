8592c62aadf73f31d862de3e3029a31bbf1e7413
wasabina67 <wasabina67@gmail.com>
2025-07-30 08:48:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from moviepy import AudioFileClip, concatenate_audioclips, ImageClip
+
+
 def main():
-    print("Hello from moviepy-example!")
+    audio_files = ["1.mp3", "2.mp3", "3.mp3"]
+    image_file = "1.png"
+    new_size = (1920, 1080)
+    output_file = "output.mp4"
+
+    clips = [AudioFileClip(f) for f in audio_files]
+    combined = concatenate_audioclips(clips)
+    image_clip = ImageClip(image_file).resized(new_size).with_duration(combined.duration)  # noqa
+    final = image_clip.with_audio(combined)
+    final.write_videofile(
+        output_file,
+        codec="libx264",
+        audio_codec="aac",
+        fps=1,
+    )
 
 
 if __name__ == "__main__":
